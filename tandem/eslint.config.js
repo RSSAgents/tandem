85fa53b2984f20e6933c19e4748d438ba7ca07d2
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-02-11 13:10:57 +0500

    chore: add eslint plugins for imports

--- tandem/eslint.config.js
+++ tandem/eslint.config.js
@@ -1,13 +1,37 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
+import perfectionist from 'eslint-plugin-perfectionist'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
+
+    plugins: {
+      import: importPlugin,
+      'unused-imports': unusedImports,
+      perfectionist: perfectionist,
+    },
+
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
     ],
+
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+
+      'import/order': [
+        'warn',
+        { 'newlines-between': 'always' },
+      ],
+
+      'perfectionist/sort-imports': [
+        'warn',
+        { type: 'natural', order: 'asc' },
+      ],
+    },
   },
 ])
