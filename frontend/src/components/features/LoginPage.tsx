fc2495b5e01a8497b925ccb41c94734a78e392e9
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-02-25 17:36:24 +0500

    feat: add module.css for login page

--- frontend/src/components/features/LoginPage.tsx
+++ frontend/src/components/features/LoginPage.tsx
@@ -1,12 +1,25 @@
 import { Button, Paper, PasswordInput, Text, TextInput } from '@mantine/core';
+import classes from './LoginPage.module.css';
 
 export const LoginPage = () => {
   return (
-    <Paper>
-      <Text>Tandem</Text>
-      <TextInput label="Email" placeholder="you@example.com" />
-      <PasswordInput label="Password" placeholder="••••••••" />
-      <Button>LOGIN</Button>
+    <Paper className={classes.card}>
+      <Text className={classes.title}>Tandem</Text>
+      <TextInput
+        radius="md"
+        label="Email"
+        placeholder="you@example.com"
+        classNames={{ input: classes.input, label: classes.label }}
+      />
+      <PasswordInput
+        radius="md"
+        label="Password"
+        placeholder="••••••••"
+        classNames={{ input: classes.input, label: classes.label }}
+      />
+      <Button radius="md" className={classes.button} fullWidth>
+        LOGIN
+      </Button>
       <Text size="sm" mt="md" c="dimmed">
         Forgot password?
       </Text>
