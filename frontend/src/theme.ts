8bfcb9450447601c0c5dbd40ec7c1a1ae074f774
solarsungai <solarsungai@gmail.com>
2026-02-26 17:33:51 +0200

    feat: improve design and add team members descriptions

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -4,4 +4,12 @@ export const theme = createTheme({
   fontFamily: 'Inter, sans-serif',
   primaryColor: 'blue',
   cursorType: 'pointer',
+  headings: {
+    fontFamily: 'Inter, sans-serif',
+    fontWeight: '800',
+    sizes: {
+      h1: { fontSize: '3rem', lineHeight: '1.2' },
+      h3: { fontSize: '1.5rem', lineHeight: '1.3' },
+    },
+  },
 });
