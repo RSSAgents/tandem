61de8ddae3eea13f79e400babb58892e7227a106
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-02-26 22:07:50 +0500

    feat: add validation and form logic to login page

--- frontend/src/components/features/auth/LoginPage.tsx
+++ frontend/src/components/features/auth/LoginPage.tsx
@@ -1,10 +1,24 @@
 import { useTheme } from '@hooks/useTheme';
 import { ActionIcon, Button, Paper, PasswordInput, Text, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { IconMoon, IconSun } from '@tabler/icons-react';
 import classes from './LoginPage.module.css';
 
 export const LoginPage = () => {
   const { handleToggleTheme, isDark } = useTheme();
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value) => (value.length >= 6 ? null : 'Password must be at least 6 characters'),
+    },
+    validateInputOnBlur: true,
+  });
+  const handleSubmit = () => {};
+
   return (
     <Paper className={classes.card}>
       <ActionIcon
@@ -17,21 +31,25 @@ export const LoginPage = () => {
         {isDark ? <IconSun size="1rem" /> : <IconMoon size="1rem" />}
       </ActionIcon>
       <Text className={classes.title}>Tandem</Text>
-      <TextInput
-        radius="md"
-        label="Email"
-        placeholder="you@example.com"
-        classNames={{ input: classes.input, label: classes.label }}
-      />
-      <PasswordInput
-        radius="md"
-        label="Password"
-        placeholder="••••••••"
-        classNames={{ input: classes.input, label: classes.label }}
-      />
-      <Button radius="md" className={classes.button} fullWidth>
-        LOGIN
-      </Button>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <TextInput
+          radius="md"
+          label="Email"
+          placeholder="you@example.com"
+          className={classes.input}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          radius="md"
+          label="Password"
+          placeholder="••••••"
+          className={classes.input}
+          {...form.getInputProps('password')}
+        />
+        <Button radius="md" fullWidth type="submit" className={classes.button}>
+          LOGIN
+        </Button>
+      </form>
       <Text size="sm" mt="md" c="dimmed">
         Forgot password?
       </Text>
