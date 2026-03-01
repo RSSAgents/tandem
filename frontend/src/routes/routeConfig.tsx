10730bac7f8145ac86fda068731f3bb6352cc4ee
Marta <emeraldraven27@gmail.com>
2026-03-02 02:16:28 +0300

    refactor: update routes according to both layouts

--- frontend/src/routes/routeConfig.tsx
+++ frontend/src/routes/routeConfig.tsx
@@ -1,14 +1,23 @@
 import { MainLayout } from '@components/layouts/MainLayout/MainLayout';
+import { MinimalLayout } from '../components/layouts/MinimalLayout/MinimalLayout';
 import { ROUTE_PATHS } from './routePaths';
 
 export const appRoutes = [
   {
     path: '/',
-    element: <MainLayout />,
+    element: <MinimalLayout />,
     children: [
       { index: true, element: <p>HomePage</p> },
       { path: ROUTE_PATHS.LOGIN, element: <p>LoginPage</p> },
-      { path: ROUTE_PATHS.NOT_FOUND, element: <p>NotFoundPage</p> },
+      { path: ROUTE_PATHS.NOT_FOUND, element: <p>Not Found</p> },
+    ],
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      { path: ROUTE_PATHS.DASHBOARD, element: <p>Dashboard</p> },
+      { path: ROUTE_PATHS.LIBRARY, element: <p>Library Page</p> },
+      { path: ROUTE_PATHS.ACHIEVEMENTS, element: <p>Achievements Page</p> },
     ],
   },
 ];
