7612f545a662c20eade109f3296bd59d5d82add9
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-03-02 01:38:05 +0500

    feat: implement login page using react hook form with zod and add tranlation

--- frontend/src/components/features/auth/LoginPage.tsx
+++ frontend/src/components/features/auth/LoginPage.tsx
@@ -1,59 +1,67 @@
 import { useTheme } from '@hooks/useTheme';
 import { ActionIcon, Button, Paper, PasswordInput, Text, TextInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
 import { IconMoon, IconSun } from '@tabler/icons-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema, LoginFormValues } from './login.schema';
 import classes from './LoginPage.module.css';
+import { useNavigate } from 'react-router-dom';
+import { LanguageSwitcher } from '@components/layouts/Header/LanguageSwitcher';
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
   const { handleToggleTheme, isDark } = useTheme();
-  const form = useForm({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-      password: (value) => (value.length >= 6 ? null : 'Password must be at least 6 characters'),
-    },
-    validateInputOnBlur: true,
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onBlur',
   });
-  const handleSubmit = () => {};
+
+  const onSubmit = async () => {
+    navigate('/dashboard');
+  };
 
   return (
     <Paper className={classes.card}>
-      <ActionIcon
-        onClick={handleToggleTheme}
-        variant="subtle"
-        radius="md"
-        size="lg"
-        className={classes.themeToggle}
-      >
-        {isDark ? <IconSun size="1rem" /> : <IconMoon size="1rem" />}
-      </ActionIcon>
-      <Text className={classes.title} mb="lg">
+      <div className={classes.topControls}>
+        <LanguageSwitcher />
+
+        <ActionIcon onClick={handleToggleTheme} variant="default" size="xl" radius="md">
+          {isDark ? <IconSun size="1rem" /> : <IconMoon size="1rem" />}
+        </ActionIcon>
+      </div>
+
+      <Text size="2xl" fw={600} ta="center" mb="xl" c="brand.5">
         Tandem
       </Text>
-      <form onSubmit={form.onSubmit(handleSubmit)}>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
         <TextInput
-          radius="md"
           label="Email"
           placeholder="you@example.com"
-          className={classes.input}
-          {...form.getInputProps('email')}
-          mb="md"
+          {...register('email')}
+          error={errors.email?.message}
+          mb="lg"
         />
+
         <PasswordInput
-          radius="md"
           label="Password"
           placeholder="••••••"
-          className={classes.input}
-          {...form.getInputProps('password')}
+          {...register('password')}
+          error={errors.password?.message}
+          mb="xl"
         />
-        <Button radius="md" mt="lg" fullWidth type="submit" className={classes.button}>
+
+        <Button type="submit" fullWidth loading={isSubmitting}>
           LOGIN
         </Button>
       </form>
-      <Text size="sm" mt="md" c="dimmed">
+
+      <Text size="sm" mt="lg" c="dimmed">
         Forgot password?
       </Text>
     </Paper>
