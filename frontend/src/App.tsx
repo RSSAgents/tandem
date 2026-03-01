aae86f9b75d8338a87a674b7586faa20847a59ee
solarsungai <solarsungai@gmail.com>
2026-03-01 07:07:05 +0200

    refactor: delete MantineProveder from App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import { router } from '@/routes/index';
-import { MantineProvider } from '@mantine/core';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import { theme } from './theme';
 
 function App() {
-  return (
-    <MantineProvider theme={theme} defaultColorScheme="dark">
-      <RouterProvider router={router} />
-    </MantineProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
