78a64eaf9d60871d5227db9c1c86b76dd5b19055
Marta <emeraldraven27@gmail.com>
2026-03-03 02:14:58 +0300

    refactor: edit main layout styles

--- frontend/src/components/layouts/MainLayout/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,17 +1,16 @@
 import { AppShell } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
+import { LAYOUT_CONFIG } from '../../../constants/layout';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 import { Sidebar } from '../Sidebar/Sidebar';
 import classes from './MainLayout.module.css';
-import { LAYOUT_CONFIG } from '../../../constants/layout';
 
 export const MainLayout = () => {
   return (
     <AppShell
       header={{ height: LAYOUT_CONFIG.HEADER_HEIGHT }}
       footer={{ height: LAYOUT_CONFIG.FOOTER_HEIGHT }}
-      padding={0}
     >
       <AppShell.Header withBorder={false} px="var(--app-side-padding)">
         <Header />
