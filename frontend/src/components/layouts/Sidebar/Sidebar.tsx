5970caf9881e297142d750d05ed968a00d8141a6
Marta <emeraldraven27@gmail.com>
2026-03-02 22:30:12 +0300

    feat: add user block

--- frontend/src/components/layouts/Sidebar/Sidebar.tsx
+++ frontend/src/components/layouts/Sidebar/Sidebar.tsx
@@ -1,11 +1,15 @@
 import { Box, Stack } from '@mantine/core';
+import { UserBlock } from '../../ui/UserBlock/UserBlock';
 import classes from './Sidebar.module.css';
 import { SidebarNavigation } from './SidebarNavigation';
 
 export const Sidebar = () => {
+  const user = { name: 'Alex', rank: 'Mage' };
+
   return (
     <Stack className={classes.sidebar}>
       <Box>
+        <UserBlock name={user.name} rank={user.rank} />
         <SidebarNavigation />
       </Box>
     </Stack>
