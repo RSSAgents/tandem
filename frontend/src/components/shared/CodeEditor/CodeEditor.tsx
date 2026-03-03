3dacc5a157c6d24b2c38170f28adbe8432b2d76a
ILYA <106765406+D15ND@users.noreply.github.com>
2026-03-04 01:10:50 +0300

    feat: add save code from editor in store

--- frontend/src/components/shared/CodeEditor/CodeEditor.tsx
+++ frontend/src/components/shared/CodeEditor/CodeEditor.tsx
@@ -1,15 +1,32 @@
+import { RootState } from '@/store';
 import { Box, Container } from '@mantine/core';
 import MonacoEditor from '@monaco-editor/react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CodeEditor.module.css';
 import { CodeEditorConfig } from './CodeEditorConfig';
+import { setCodeEditor } from './slice/editorSlice';
 
 export const CodeEditor = () => {
   const config = CodeEditorConfig();
+  const dispatch = useDispatch();
+  const editorCode = useSelector((state: RootState) => state.editorStore.code);
+
+  const handleEditorChange = (code: string | undefined) => {
+    dispatch(setCodeEditor(code || ''));
+  };
+
+  // Clear editor window after go out from page
+  useEffect(() => {
+    return () => {
+      dispatch(setCodeEditor(''));
+    };
+  }, [dispatch]);
 
   return (
     <Container className={classes.container}>
       <Box className={classes.block}>
-        <MonacoEditor {...config} />
+        <MonacoEditor {...config} value={editorCode} onChange={handleEditorChange} />
       </Box>
     </Container>
   );
