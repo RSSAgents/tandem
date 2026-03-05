d0429f5a4ccc767ece79671f86d784df4319bc93
solarsungai <solarsungai@gmail.com>
2026-03-05 18:39:47 +0200

    fix: fix tests

--- frontend/src/routes/routeConfig.tsx
+++ frontend/src/routes/routeConfig.tsx
@@ -1,6 +1,7 @@
 import { About } from '@/pages/About/About';
 import { MainLayout } from '@components/layouts/MainLayout/MainLayout';
 import { lazy } from 'react';
+import { NotFoundPage } from '../pages/NotFoundPage/NotFoundPage';
 import { ROUTE_PATHS } from './routePaths';
 
 const LoginPage = lazy(() => import('@components/features/auth/LoginPage'));
@@ -13,7 +14,7 @@ export const appRoutes = [
       { index: true, element: <p>HomePage</p> },
       { path: ROUTE_PATHS.LOGIN, element: <LoginPage /> },
       { path: ROUTE_PATHS.ABOUT, element: <About /> },
-      { path: ROUTE_PATHS.NOT_FOUND, element: <p>NotFoundPage</p> },
+      { path: ROUTE_PATHS.NOT_FOUND, element: <NotFoundPage /> },
     ],
   },
 ];
