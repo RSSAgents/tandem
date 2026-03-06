ee00fd846b1a0dd81037742c820617d2c4beac61
Marta <emeraldraven27@gmail.com>
2026-03-06 22:00:54 +0300

    refactor: edit main layout structure

--- frontend/src/components/layouts/MainLayout/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout/MainLayout.tsx
@@ -9,23 +9,22 @@ import classes from './MainLayout.module.css';
 export const MainLayout = () => {
   return (
     <AppShell
+      layout="alt"
       header={{ height: LAYOUT_CONFIG.HEADER_HEIGHT }}
       footer={{ height: LAYOUT_CONFIG.FOOTER_HEIGHT }}
+      navbar={{ width: LAYOUT_CONFIG.SIDEBAR_WIDTH, breakpoint: 'sm' }}
+      padding={0}
     >
+      <AppShell.Navbar withBorder={false}>
+        <Sidebar />
+      </AppShell.Navbar>
+
       <AppShell.Header withBorder={false} px="var(--app-side-padding)">
         <Header />
       </AppShell.Header>
 
       <AppShell.Main className={classes.main}>
-        <div className={classes.appBoard}>
-          <aside className={classes.sidebarSection}>
-            <Sidebar />
-          </aside>
-
-          <section className={classes.contentSection}>
-            <Outlet />
-          </section>
-        </div>
+        <Outlet />
       </AppShell.Main>
 
       <AppShell.Footer withBorder={false} px="var(--app-side-padding)">
