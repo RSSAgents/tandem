4524e4adaeacf5cbbe96a13d7d14cf3e8a6b3a33
ILYA <106765406+D15ND@users.noreply.github.com>
2026-03-09 19:50:08 +0300

    feat: basic styles

--- frontend/src/components/features/widgets/Stack/Stack.tsx
+++ frontend/src/components/features/widgets/Stack/Stack.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Title } from '@mantine/core';
+import { Box, Container, Text, Title } from '@mantine/core';
 import classes from './Stack.module.css';
 
 const StackWidget = () => {
@@ -7,7 +7,48 @@ const StackWidget = () => {
       <Title className={classes.title} order={1}>
         Stack
       </Title>
-      <Box className={classes.block}></Box>
+      <Box className={classes.block}>
+        <Box className={classes.container_lifo}>
+          <Box className={classes.books}>
+            <Box className={classes.book}>
+              <Text>5</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>4</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>3</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>2</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>1</Text>
+            </Box>
+          </Box>
+          <Box className={classes.cup}></Box>
+        </Box>
+        <Box className={classes.container_fifo}>
+          <Box className={classes.cup}></Box>
+          <Box className={classes.books}>
+            <Box className={classes.book}>
+              <Text>5</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>4</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>3</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>2</Text>
+            </Box>
+            <Box className={classes.book}>
+              <Text>1</Text>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </Container>
   );
 };
