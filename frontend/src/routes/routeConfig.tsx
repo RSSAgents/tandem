4b76f2262da17f150f6e988f1e155cbf6bf07bd7
ILYA <106765406+D15ND@users.noreply.github.com>
2026-03-14 02:38:26 +0300

    refactor: change routing way

--- frontend/src/routes/routeConfig.tsx
+++ frontend/src/routes/routeConfig.tsx
@@ -19,7 +19,6 @@ export const appRoutes = [
       { index: true, element: <p>HomePage</p> },
       { path: ROUTE_PATHS.LOGIN, element: <LoginPage /> },
       { path: ROUTE_PATHS.ABOUT, element: <About /> },
-      { path: ROUTE_PATHS.STACK_WIDGET, element: <StackWidget /> },
       { path: ROUTE_PATHS.NOT_FOUND, element: <NotFoundPage /> },
     ],
   },
@@ -30,6 +29,7 @@ export const appRoutes = [
       { path: ROUTE_PATHS.LIBRARY, element: <p>Library Page</p> },
       { path: ROUTE_PATHS.ACHIEVEMENTS, element: <p>Achievements Page</p> },
       { path: ROUTE_PATHS.WIDGET_CONSOLE, element: <WidgetConsole /> },
+      { path: ROUTE_PATHS.STACK_WIDGET, element: <StackWidget /> },
     ],
   },
 ];
