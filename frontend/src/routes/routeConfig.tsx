21ccbffad5513d2a45af2253f6aedf32cff37b44
vika oneil <oneilcode111@gmail.com>
2026-03-14 00:33:15 +1000

    feat: add path for leaderboard

--- frontend/src/routes/routeConfig.tsx
+++ frontend/src/routes/routeConfig.tsx
@@ -5,7 +5,7 @@ import { lazy } from 'react';
 import { MinimalLayout } from '@components/layouts/MinimalLayout/MinimalLayout';
 import { ROUTE_PATHS } from './routePaths';
 import { NotFoundPage } from '@/pages/NotFoundPage/NotFoundPage';
-import { LeaderboardPage } from '@/pages/LeaderboardPage/LeaderboardPage';
+import { LeaderboardPage } from '../pages/LeaderboardPage/LeaderboardPage';
 
 const LoginPage = lazy(() => import('@/components/features/auth/LoginPage'));
 
@@ -16,17 +16,17 @@ export const appRoutes = [
     children: [
       { index: true, element: <p>HomePage</p> },
       { path: ROUTE_PATHS.LOGIN, element: <LoginPage /> },
+      { path: ROUTE_PATHS.ABOUT, element: <About /> },
       { path: ROUTE_PATHS.NOT_FOUND, element: <NotFoundPage /> },
-      { path: ROUTE_PATHS.WIDGET_CONSOLE, element: <WidgetConsole /> },
     ],
   },
   {
     element: <MainLayout />,
     children: [
       { path: ROUTE_PATHS.DASHBOARD, element: <p>Dashboard</p> },
-      { path: ROUTE_PATHS.ABOUT, element: <About /> },
       { path: ROUTE_PATHS.LIBRARY, element: <p>Library Page</p> },
       { path: ROUTE_PATHS.ACHIEVEMENTS, element: <p>Achievements Page</p> },
+      { path: ROUTE_PATHS.WIDGET_CONSOLE, element: <WidgetConsole /> },
       { path: ROUTE_PATHS.LEADERBOARD_PAGE, element: <LeaderboardPage /> },
     ],
   },
