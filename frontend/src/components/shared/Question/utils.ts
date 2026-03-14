2a1cce707eeffa251a96f290c465083c73600203
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-03-15 00:06:42 +0500

    refactor: replace duplicated controlled state logic with hook

--- frontend/src/components/shared/Question/utils.ts
+++
@@ -1,9 +0,0 @@
-export function updateControlledState<T>(
-  externalValue: T | undefined,
-  setState: (value: T) => void,
-  newValue: T,
-) {
-  if (externalValue === undefined) {
-    setState(newValue);
-  }
-}
