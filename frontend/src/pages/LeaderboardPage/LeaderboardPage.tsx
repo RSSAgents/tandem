9dad4a6c2ea4e12b9822e0871c89c8c2b5795891
vika oneil <oneilcode111@gmail.com>
2026-03-14 21:56:12 +1000

    fix: move mock data to Leaderboard component

--- frontend/src/pages/LeaderboardPage/LeaderboardPage.tsx
+++ frontend/src/pages/LeaderboardPage/LeaderboardPage.tsx
@@ -3,8 +3,48 @@ import { Avatar, Group, ScrollArea, Table, Text, Paper, Stack, Container } from 
 import { ProgressBar } from '@/components/features/ProgressBar/ProgressBar';
 import classes from './LeaderboardPage.module.css';
 import { WINNERS_TABLE_HEADERS } from '@/constants/winnerTableHeaders';
-import { sortedByScoreData } from '@/utils/leaderboardPage.utils';
-import { RANK_DISPLAY } from '../../constants/rankDisplay';
+import { RANK_DISPLAY } from '@/constants/rankDisplay';
+import { Winner } from '@/types/winner.types';
+
+const data: Winner[] = [
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png',
+    name: 'Ivan Ivanov',
+    score: 10,
+    widgetsAmount: { completed: 2, notCompleted: 3 },
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-7.png',
+    name: 'Marge',
+    score: 20,
+    widgetsAmount: { completed: 3, notCompleted: 2 },
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png',
+    name: 'Liza',
+    score: 30,
+    widgetsAmount: { completed: 4, notCompleted: 1 },
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-3.png',
+    name: 'Bart',
+    score: 40,
+    widgetsAmount: { completed: 5, notCompleted: 0 },
+  },
+  {
+    avatar:
+      'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-10.png',
+    name: 'Homer',
+    score: 50,
+    widgetsAmount: { completed: 2, notCompleted: 3 },
+  },
+];
+
+const sortedByScoreData = [...data].sort((a, b) => b.score - a.score);
 
 export const LeaderboardPage = () => {
   const { t } = useTranslation('leaderboard');
