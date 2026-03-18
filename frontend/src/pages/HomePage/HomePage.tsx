294974e502173a687fff5ae48af76dc7eda84bf7
vika oneil <oneilcode111@gmail.com>
2026-03-19 00:51:05 +1000

    feat: add container for Home Page

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mantine/core';
 import { AboutAISection } from './sections/AboutAISection/AboutAISection';
 import { AboutWidgetsSection } from './sections/AboutWidgetsSection/AboutWidgetsSection';
 import { AudienceSection } from './sections/AudienceSection/AudienceSection';
@@ -6,10 +7,11 @@ import { FAQSection } from './sections/FAQSection/FAQSection';
 import { HeroSection } from './sections/HeroSection/HeroSection';
 import { HowItWorksSection } from './sections/HowItWorksSection/HowItWorksSection';
 import { LeaderboardSection } from './sections/LeaderboardSection/LeaderboardSection';
+import classes from './HomePage.module.css';
 
 export function HomePage() {
   return (
-    <>
+    <Box className={classes.container}>
       <HeroSection />
       <HowItWorksSection />
       <AudienceSection />
@@ -18,6 +20,6 @@ export function HomePage() {
       <LeaderboardSection />
       <BenefitsSection />
       <FAQSection />
-    </>
+    </Box>
   );
 }
