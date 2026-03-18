c21674bb10a6c70197bd25d7e2dae5c6279b00fe
vika oneil <oneilcode111@gmail.com>
2026-03-19 00:52:37 +1000

    feat: add conctsnts to Hero section to avoid test in code + add styles

--- frontend/src/pages/HomePage/sections/HeroSection/HeroSection.tsx
+++ frontend/src/pages/HomePage/sections/HeroSection/HeroSection.tsx
@@ -1,18 +1,31 @@
 import { Button, Container, Text, Title } from '@mantine/core';
 import classes from './HeroSection.module.css';
+import { IconArrowDown } from '@tabler/icons-react';
+
+const TITLE = 'Your Skills. Tracked. Improved. Mastered.';
+const SUB_TITLE = `Interactive widget-based challenges & AI-powered mock interviews - practice with real
+          questions, get instant feedback and track your progress with Tandem`;
+const BTN_TEXT = 'Learn more';
 
 export function HeroSection() {
   return (
-    <Container className={classes.wrapper}>
-      <Title className={classes.title}>Your Skills. Tracked. Improved. Mastered.</Title>
+    <Container className={classes.section}>
+      <Title order={1} className={classes.title}>
+        {TITLE}
+      </Title>
       <Container p={0} size={600}>
         <Text size="lg" c="dimmed" className={classes.subtitle}>
-          Interactive widget-based challenges & AI-powered mock interviews - practice with real
-          questions, get instant feedback and track your progress with Tandem
+          {SUB_TITLE}
         </Text>
       </Container>
-      <Button className={classes.control} size="lg" variant="default" color="gray">
-        Learn more
+      <Button
+        className={classes.learnMoreBtn}
+        size="lg"
+        variant="default"
+        color="gray"
+        rightSection={<IconArrowDown size={18} />}
+      >
+        {BTN_TEXT}
       </Button>
     </Container>
   );
