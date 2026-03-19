37cf3880d690b05c36418e221e3d2557bf38e482
ILYA <106765406+D15ND@users.noreply.github.com>
2026-03-19 03:24:33 +0300

    refactor: add way to i18n

--- frontend/src/i18n/locales/en/index.ts
+++ frontend/src/i18n/locales/en/index.ts
@@ -4,7 +4,7 @@ import header from './header.json';
 import notFound from './notFound.json';
 import shared from './shared.json';
 import leaderboard from './leaderboard.json';
-import library from './library.json'
+import library from './library.json';
 
 export const en = {
   auth,
